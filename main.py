cb35c7a05d514656e8a4d6edaedcd578fa61b969
TestadeNevill <64296826+TestadeNevill@users.noreply.github.com>
2022-04-07 23:43:57 -0400

    looping through jokes objects

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 import pyttsx3
 import requests
-# import json
+import json
 
 
 engine = pyttsx3.init()
@@ -11,24 +11,21 @@ payload = {'type': 'twopart'}
 response = requests.get("https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,"
                         "explicit&type=twopart&amount=2", params=payload)
 
-# engine.say(response.json())
 
-print(response.json())
-
-#
-# jokes_data = response.
-# json_data = json.loads(jokes_data)
-# print(json_data)
+joke_data = response.json()
 
 
-class Joke:
+engine.say(joke_data)
 
-    def __init__(self, setup, delivery) -> None:
-        self.setup = setup
-        self.delivery = delivery
-
-    def __str__(self) -> str:
-        return f"setup {self.setup} delivery {self.delivery}"
+for i in joke_data['jokes']:
+    print(i)
+pyttsx3.speak(joke_data)
 
 
-# pyttsx3.speak(Joke)
+
+
+
+
+
+
+
